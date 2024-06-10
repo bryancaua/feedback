e80556ac3067e3b505e38d543f8c318ab4c90cc7
bryan caua <bryanncaua@gmail.com>
2024-06-10 01:04:19 +0000

    style: estilização completa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,20 @@
-import { ButtonContainer, CardContainer, IconContainer } from "./Home.styles";
+import { ButtonContainer, CardContainer, IconContainer, ResultContainer } from "./Home.styles";
+
+
 import iconStarImg from "../assets/icon-star.svg"
+import thankYouImg from "../assets/illustration-thank-you.svg"
 
 
 export function Home() {
+    let aparecerResultado = true
+
   return (
+   aparecerResultado === false ? ( 
+    
     <CardContainer>
-        <IconContainer>
-            <img src={iconStarImg} alt="icone de estrela" />
-        </IconContainer>
+            <IconContainer>
+                <img src={iconStarImg} alt="icone de estrela" />
+            </IconContainer>
 
         <h1>Como foi o atendimento?</h1>
         <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
@@ -20,8 +27,19 @@ export function Home() {
             <button>5</button>
         </ButtonContainer>
 
-        
+
         <button>Enviar</button>
     </CardContainer>
-  )
+
+    ) : (<CardContainer>
+        <img src={thankYouImg} alt="imagem de agradecimento" />
+
+        <ResultContainer><p>Você selecionou 4 de 5</p></ResultContainer>
+
+        <h1>Obrigado!</h1>
+
+        <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suportem, não hesite em entrar em contato!</p>
+        </CardContainer>
+     )
+  ) 
 }
